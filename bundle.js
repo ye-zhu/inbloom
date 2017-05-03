9609936451da41bd111527283c9f689c619969f8
ye-zhu <z.yeqin@gmail.com>
2017-05-03 01:03:20 -0400

    changed explosion graphics

--- bundle.js
+++ bundle.js
@@ -547,10 +547,10 @@
 	    frameWidth: 256,
 	    frameHeight: 256,
 	    offset: 100,
-	    totalFrameX: 1280,
-	    totalFrameY: 1792,
-	    imageWidth: 200,
-	    imageHeight: 200
+	    totalFrameX:1280,
+	    totalFrameY:1792,
+	    imageWidth:200,
+	    imageHeight:200
 	  },
 	
 	  // 1280 × 2304
